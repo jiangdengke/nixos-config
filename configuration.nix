c60147f7c2e5d58dc053e2efce2ec18c98901caf
jiangdk <jiang1728439852@gmail.com>
2025-07-31 20:43:30 +0800

    add something

--- configuration.nix
+++ configuration.nix
@@ -35,25 +35,71 @@ networking = {
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n = {
-	defaultLocale = "zh_CN.UTF-8";
-  };
-  fonts = {
-  	enableDefaultPackages = true;
-	packages = with pkgs; [
-		noto-fonts
-		noto-fonts-cjk-sans
-		noto-fonts-emoji
-		noto-fonts-extra
-		jetbrains-mono
-		wqy_microhei
-		wqy_zenhei
-		xorg.fontadobe75dpi
-		nerd-font-patcher
-        noto-fonts-color-emoji
-	];
 
+  environment.variables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
   };
+nixpkgs.overlays = [
+    (final: prev: {
+      librime = (prev.librime.override {
+        plugins = [
+#           (pkgs.fetchFromGitHub {
+#             owner = "hchunhui";
+#             repo = "librime-lua";
+#             rev = "e3912a4b3ac2c202d89face3fef3d41eb1d7fcd6";
+#             sha256 = "sha256-zx0F41szn5qlc2MNjt1vizLIsIFQ67fp5cb8U8UUgtY=";
+#           })
+            pkgs.librime-lua
+            pkgs.librime-octagram
+        ];
+      }).overrideAttrs (old: {
+        buildInputs = (old.buildInputs or []) ++ [pkgs.luajit]; # 用luajit
+#         buildInputs = (old.buildInputs or []) ++ [pkgs.lua5_4]; # 用lua5.4
+      });
+    })
+  ];
+
+#   i18n.inputMethod.enable = false;
+  i18n = {
+   defaultLocale = "zh_CN.UTF-8";
+   inputMethod = {
+     
+    type = "fcitx5";
+    enable = true;
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      rime-data # 如果你不需要内置数据可以注释掉，我就注释掉了
+      fcitx5-gtk
+      kdePackages.fcitx5-qt
+      fcitx5-rime
+      fcitx5-nord # 主题
+#       fcitx5-material-color # 主题
+    ];
+  };
+};
+
+
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      noto-fonts-extra
+      jetbrains-mono
+      wqy_microhei
+      wqy_zenhei
+      xorg.fontadobe75dpi
+      nerd-font-patcher
+      noto-fonts-color-emoji
+    ];  # fonts 的闭合分号
+  };  # fonts 的闭合花括号
+
+
+
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
@@ -126,7 +172,7 @@ localsend
 neovim
 daed
 qq
-wechat
+wechat-uos
 
 networkmanager
 networkmanagerapplet
