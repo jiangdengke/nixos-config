5b937c329d3c90a599bc41bc8e57aa807a230870
jiangdengke <jiang1728439852@gmail.com>
2025-10-01 15:06:07 +0800

    update

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -7,6 +7,14 @@
   programs.git = {
     enable = true;
 
-    # ... Other options ...
+    userName = "jiangdengke";
+    userEmail = "jiang1728439852@gmail.com";
+
+    extraConfig = {
+      core.askPass = "";
+      credential.helper = "!${pkgs.gh}/bin/gh auth git-credential";
+      url."git@github.com:".insteadOf = "https://github.com/";
+      url."ssh://git@github.com/".insteadOf = "https://github.com/";
+    };
   };
 }
