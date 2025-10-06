ec2eb2e7a2c87d6a3b1eefdc1d111e3c2579f93e
jiangdengke <jiang1728439852@gmail.com>
2025-10-06 15:55:55 +0800

    update

--- home/cliphist/default.nix
+++ home/cliphist/default.nix
@@ -4,21 +4,23 @@
 
   home.file.".local/bin/cliphist-rofi.sh" = {
     force = true;
-    source = pkgs.substituteAll {
+    source = pkgs.replaceVars {
       src = ./cliphist-rofi.sh;
-      cliphist = "${pkgs.cliphist}/bin/cliphist";
-      wl_copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+      replacements = {
+        cliphist = "${pkgs.cliphist}/bin/cliphist";
+        wl_copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+      };
     };
-    executable = true;
   };
 
   home.file.".local/bin/cliphist-daemon.sh" = {
     force = true;
-    source = pkgs.substituteAll {
+    source = pkgs.replaceVars {
       src = ./cliphist-daemon.sh;
-      cliphist = "${pkgs.cliphist}/bin/cliphist";
-      wl_paste = "${pkgs.wl-clipboard}/bin/wl-paste";
+      replacements = {
+        cliphist = "${pkgs.cliphist}/bin/cliphist";
+        wl_paste = "${pkgs.wl-clipboard}/bin/wl-paste";
+      };
     };
-    executable = true;
   };
 }
